7e93d6e81b641a3c2d3b9ba309b696c605f254fc
Dokas <iguinhogms@gmail.com>
2023-06-23 02:22:01 -0300

    Inicialização do projeto, comecando tambem a fazer a estruturacao de rotas

---
+++ src/app/clientes/page.tsx
@@ -0,0 +1,7 @@
+export default function ClientesPage(): JSX.Element {
+  return (
+    <div>
+      <p>Olá</p>
+    </div>
+  );
+}
---
+++ src/app/produtos/page.tsx
@@ -0,0 +1,7 @@
+export default function ProdutosPage(): JSX.Element {
+  return (
+    <div>
+      <p>Produtos</p>
+    </div>
+  );
+}
